ead2533651cecf67fd497fedf93f796fc4f2b188
Dan <12995058+danielhac@users.noreply.github.com>
2020-05-25 16:54:03 -0700

    first commit

---
+++ public/js/chat.js
@@ -0,0 +1,76 @@
+const socket = io()
+
+// Form elements
+const $messageForm = document.getElementById('message-form')
+const $messageFormInput = $messageForm.getElementsByTagName('input')[0]
+const $messageFormButton = $messageForm.getElementsByTagName('button')[0]
+const $messages = document.getElementById('messages')
+
+// Templates
+const messageTemplate = document.getElementById('message-template').innerHTML
+const sidebarTemplate = document.getElementById('sidebar-template').innerHTML
+
+// Username and Room
+const username = location.search.substr(1).split('&')[0].split('=')[1]
+const room =     location.search.substr(1).split('&')[1].split('=')[1]
+
+// Autoscroll
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+socket.on('message', (msg) => {
+    const html = Mustache.render(messageTemplate, {
+        username: msg.username,
+        msg: msg.text,
+        createdAt: moment(msg.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.getElementById('sidebar').innerHTML = html
+})
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    $messageFormButton.setAttribute('disabled', 'disabled')
+    socket.emit('sendMessage', e.target.elements.message.value, (acknowledge) => {
+        console.log('Message delivered', acknowledge);
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+    })
+})
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
---
+++ src/index.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const { generateMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirPath))
+
+io.on('connection', (socket) => {
+    console.log('Websocket connection');
+    
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room })
+        if (error) { return callback(error) }
+
+
+        socket.join(user.room)
+        socket.emit('message', generateMessage('Dans Bot', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Dans Bot', `${user.username} joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+        callback()
+    })
+    
+    socket.on('sendMessage', (msg, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('message', generateMessage(user.username, msg))
+        callback('Delivered')
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Dans Bot', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server up on port: ${port}`);
+})
